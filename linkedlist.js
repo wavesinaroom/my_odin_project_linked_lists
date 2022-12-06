28badbc099a6c2554e0b0bec9b7f1dc4a6ade6b2
wavesinaroom <juan@wavesinaroom.com>
2022-12-06 10:26:37 -0500

    [Feature] Return node count with countItems function. Node turns into a class to be able to use a static count variable

--- linkedlist.js
+++ linkedlist.js
@@ -1,46 +1,43 @@
-function createNode(value){
-  return{
-    value: value,
-    next: null,
-    previous: null,
+class Node{
+  static count = 0;
+  constructor(value){
+    ++Node.count;
+    this.value = value;
+    this.next = null;
+    this.previous = null;
   }
 }
 
-function List(value){
-
-  function checkPreviousNode(node){
-   if(node.previous!==null){
-     return checkPreviousNode(node.previous);
-   }else{
-     return node;
-   }
-  }
-  function checkNextNode(node){
-   if(node.next!==null){
-     return checkNextNode(node.next);
-   }else{
-     return node;
-   }
-  }
-
-  return{
-    root: createNode(value),
-    appendNode: function(value){
-      let head = checkNextNode(this.root);
-      head.next = createNode(value);
-    },
-    prependNode: function(value){
-      const temporal = this.root; 
-      this.root =createNode(value);
-      this.root.next = temporal;
-    },
-    getHead: function(){
-      return checkNextNode(this.root);
-    },
-    getTail: function(){
-      return this.root;
+function List(value) {
+  function checkNextNode(node) {
+    if (node.next !== null) {
+      return checkNextNode(node.next);
+    } else {
+      return node;
     }
   }
+
+  return {
+    root: new Node(value),
+    appendNode: function (value) {
+      let head = checkNextNode(this.root);
+      head.next = new Node(value);
+    },
+    prependNode: function (value) {
+      const temporal = this.root;
+      this.root = new Node(value);
+      this.root.next = temporal;
+    },
+    getHead: function () {
+      return checkNextNode(this.root);
+    },
+    getTail: function () {
+      return this.root;
+    },
+    countItems: function () {
+      return Node.count;
+    },
+  };
 }
 
 const list = List(89);
@@ -49,5 +46,4 @@ list.appendNode(9835);
 list.appendNode(8245);
 list.prependNode(9834);
 list.prependNode(2345);
-console.log(list.getHead());
-console.log(list.getTail());
+console.log(list.countItems());
